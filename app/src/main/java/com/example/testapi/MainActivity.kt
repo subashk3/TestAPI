ea096a191c9046cacff0697b7dd480afa52a859f
saravanan-selvam <43129002+saravanan-selvam@user.noreply.github.com>
2022-05-31 19:34:28 +0530

    New Commit

--- app/src/main/java/com/example/testapi/MainActivity.kt
+++ app/src/main/java/com/example/testapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.Log.d
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testapi.adapter.ApiAdapter
@@ -11,27 +12,32 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
 class MainActivity : AppCompatActivity() {
+    val BASE_URL = "https://jsonplaceholder.typicode.com/"
     lateinit var myAdapter: ApiAdapter
     lateinit var binding: ActivityMainBinding
+     val TAG = this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
+
+//        val recyclerView = binding.recyclerView
+
+//        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val service = retrofit.create(ApiInterface::class.java)
 
-        service.getDetails().enqueue(object : Callback<List<UserItem>> {
+/*        service.getDetails().enqueue(object : Callback<List<UserItem>> {
             override fun onResponse(
                 call: Call<List<UserItem>>,
                 response: Response<List<UserItem>>
@@ -43,6 +49,27 @@ class MainActivity : AppCompatActivity() {
             }
             override fun onFailure(call: Call<List<UserItem>>, t: Throwable) {
                 d("My test", t.toString())
+            }
+
+        })*/
+        service
+            .getDetails()
+            .enqueue(object : Callback<List<UserItem>> {
+            override fun onResponse(
+                call: Call<List<UserItem>>,
+                response: Response<List<UserItem>>
+            ) {
+                if (response.isSuccessful){
+                    Log.d(TAG,"API response"+ response.body().toString())
+                    myAdapter = ApiAdapter(this@MainActivity, response.body()!!)
+                    binding.recyclerView.adapter = myAdapter
+                    binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    myAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(call: Call<List<UserItem>>, t: Throwable) {
+                TODO("Not yet implemented")
             }
 
         })
--- app/src/main/java/com/example/testapi/adapter/ApiAdapter.kt
+++ app/src/main/java/com/example/testapi/adapter/ApiAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.testapi.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,18 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.testapi.R
 import com.example.testapi.UserItem
 
-class ApiAdapter(private val context: Context, private val userList: List<UserItem>) :
-    RecyclerView.Adapter<ApiAdapter.UserViewHolder>() {
+class ApiAdapter(val context: Context, private val userList: List<UserItem>) :
+    RecyclerView.Adapter<UserViewHolder>() {
+    val TAG = this.javaClass.simpleName
 
-    class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title: TextView
-        val userId: TextView
 
-        init {
-            title = view.findViewById(R.id.title_user)
-            userId = view.findViewById(R.id.user_id)
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.detail_list, parent, false)
@@ -28,13 +22,27 @@ class ApiAdapter(private val context: Context, private val userList: List<UserIt
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        holder.userId.text = userList[position].id.toString()
-        holder.title.text = userList[position].title
+        holder.addItems(userList[position])
     }
 
     override fun getItemCount(): Int {
+        Log.v(TAG, "Check getItemCount::${userList.size}")
         return userList.size
     }
 
+}
 
+class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val title: TextView
+    val userId: TextView
+
+    init {
+        title = view.findViewById(R.id.title_user)
+        userId = view.findViewById(R.id.user_id)
+    }
+
+    fun addItems(userItem: UserItem) {
+        userId.text = userItem.id.toString()
+        title.text = userItem.title
+    }
 }
